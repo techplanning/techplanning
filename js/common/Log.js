539b490248720a81b3cef246fc11b52aef0260c3
문혁중 <beyond_ps@sk.com>
2017-03-30 20:23:01 +0900

    페이지 등록

--- js/common/Log.js
+++ js/common/Log.js
@@ -68,7 +68,7 @@ function _$RAKE_cleanData() {
                 l = e.getElementsByTagName("script")[0],
                 l.parentNode.insertBefore(c, l);
             var E = t;
-            for (p = ["create", "remove", "track", "collect", "flush"],
+            for (p = ["autoTrack", "create", "remove", "track", "collect", "flush"],
                      u = 0; u < p.length; u++)
                 r(E, p[u]);
             t[i] = 1,
